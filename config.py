c39a544f72fb9eefe7bc9d6a24dcf474f9a8fbbe
Jadnast Rich <jadnostt@gmail.com>
2022-12-15 23:18:53 -0800

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import uuid
 
 # If you do not know where to get these values, then read the README.md
 
-DISCORD_TOKEN = 'MTA1MjgyNzk1MDg4NzczOTQwMg.GsToDV.E12J-jO8o0V87kYS4ioGG4XB7dHEkTW2vPrhiQ'
-DISCORD_CLIENT_ID = '1052827950887739402'
-DISCORD_CLIENT_SECRET = 'oMqqAcXtoRB9EaPaJmXbJeq_nMNMmS5U'
+DISCORD_TOKEN = ''
+DISCORD_CLIENT_ID = ''
+DISCORD_CLIENT_SECRET = ''
 DISCORD_REDIRECT_URI = 'http://localhost/discord-oauth-callback'
 COOKIE_SECRET = uuid.uuid4().hex
